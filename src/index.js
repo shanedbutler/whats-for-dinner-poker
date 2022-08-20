81a731da7f4151e60533c3123daf3bfaa9c58a8b
minidiscs <shanedbutler@gmail.com>
2022-08-20 13:47:42 -0400

    Build initial modules and structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './utils/reportWebVitals';
 import { App } from './components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
