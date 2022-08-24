a43be8afa9f0a1c3a70398b3c857ecde4c6b4218
Shane Butler <shanedbutler@gmail.com>
2022-08-24 17:08:43 -0400

    Add play result POST logic

--- src/components/play/PlayResult.js
+++ src/components/play/PlayResult.js
@@ -1,7 +1,34 @@
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { fetchHistory, getLocalUser, postOption } from "../../utils/apiUtils"
 import { ResultCard } from "./ResultCard"
 
-export const PlayResult = (props) => {
+export const PlayResult = () => {
 
+    const location = useLocation()
+    const { finalDraw } = location.state
+
+    //Initialize array to be sent to database
+    const gameResult = []
+
+    //Set object data for storage and add to array
+    finalDraw.forEach(card => {
+
+        const cardObject = {
+            cardId: card.id,
+            positionId: card.positionId,
+            timestamp: Date.now(),
+            userId: getLocalUser().id
+        }
+        gameResult.push(cardObject)
+
+    })
+
+    const postResults = () => {
+        Promise.all(gameResult.map(historyCard => fetchHistory("", postOption(historyCard))))
+    }
+
+    const navigate = useNavigate()
+    const navSelectDeck = () => navigate("/play")
 
     return (
         <>
@@ -19,10 +46,30 @@ export const PlayResult = (props) => {
                 <div className="column">
                 </div>
                 {
-                    props.finalDraw.map(card => <ResultCard key={card.id} card={card} />)
+                    finalDraw.map(card => {
+                        return <ResultCard key={card.id} card={card} />
+                    })
+
                 }
                 <div className="column">
                 </div>
+            </section>
+            <section className="is-flex is-justify-content-center">
+                <Link to="/play/round" state={{ deckId: finalDraw[0].deckId }}>
+                    <button className="button mt-1 mr-5">
+                        Play Again
+                    </button>
+                </Link>
+                <button
+                    className="button mt-1 mr-5"
+                    onClick={postResults}>
+                    Save Game
+                </button>
+                <button
+                    className="button mt-1"
+                    onClick={navSelectDeck}>
+                    Select Deck
+                </button>
             </section>
         </>
     )
