2ff25ebdb098f55d312851b8c39ce4339a6fbaf1
Shane Butler <shanedbutler@gmail.com>
2022-08-25 16:08:37 -0400

    Convert and list game with proper date string

--- src/components/history/GameItem.js
+++ src/components/history/GameItem.js
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom"
 
 export const GameItem = (props) => {
 
+    const convertedDate = new Date(props.game.timestamp)
+    const gameDate = convertedDate.toLocaleString("en-US")
+
     return (
         <Link to={`/history/${props.game.id}`}>
-            <li>{props.game.timestamp}</li>
+            <li>{gameDate}</li>
         </Link>
     )
 }
