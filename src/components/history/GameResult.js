9535288365f6b98071add69115d9232e6067dfd3
Shane Butler <shanedbutler@gmail.com>
2022-08-25 15:56:39 -0400

    Build history game result page logic

---
+++ src/components/history/GameResult.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { fetchHistory, getLocalUser } from "../../utils/apiUtils"
+import { ResultCard } from "../play/ResultCard"
+
+export const GameResult = () => {
+    const gameId = useParams().id
+    const currentUser = getLocalUser()
+    
+    const [gameHistory, setGameHistory] = useState([])
+    const [gameResult, setGameResult] = useState([])
+
+    //Find card that matches gameId passed in with useParams and use it to filter array
+    const filterArray = () => {
+        const cardGuide = gameHistory.find(card => card.id === parseInt(gameId))
+        setGameResult(gameHistory.filter(card => card.timestamp === cardGuide.timestamp))
+
+        // for (let i = (gameId); i <= (gameId + 5); i++) {
+        //     gameResult.push(gameHistory[i])
+        // }
+    }
+
+    const mapArray = () => {
+        gameResult.map(card => {
+            return <ResultCard key={card.id} card={card} />
+        })
+    }
+
+    //Get game history from database and set to state.
+    useEffect(() => {
+        fetchHistory(`?userId=${currentUser.id}`)
+            .then(historyArray => setGameHistory(historyArray))
+    }, [])
+
+    useEffect(() => {
+        filterArray()
+    }, [gameHistory])
+
+    return (
+        <section className="columns is-centered is-multiline is-2-tablet mt-5">
+            <div className="column">
+            </div>
+            {
+                mapArray()
+            }
+            <div className="column">
+            </div>
+        </section>
+    )
+}
