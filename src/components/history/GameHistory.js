42f1308ff63df44d7968605ba450d1fcdc740239
Shane Butler <shanedbutler@gmail.com>
2022-08-29 09:31:09 -0400

    Refactor to reduce use of state

--- src/components/history/GameHistory.js
+++ src/components/history/GameHistory.js
@@ -6,16 +6,12 @@ export const GameHistory = () => {
 
     const currentUser = getLocalUser()
 
-    const [gameHistory, setGameHistory] = useState([])
     const [gameList, setGameList] = useState([])
-
     const [decks, setDecks] = useState([])
 
     //Loop through gameHistory and push every 5th object to new array to make an index of games.
     const makeIndex = (arr) => {
-
         const gameIndex = []
-
         for (let i = 0; i < arr.length; i = i + 5) {
             gameIndex.push(arr[i])
         }
@@ -25,19 +21,15 @@ export const GameHistory = () => {
     //Get game history from database and set to state.
     useEffect(() => {
         fetchHistory(`?userId=${currentUser.id}&_expand=card`)
-            .then(historyArray => setGameHistory(historyArray))
+            .then(historyArray => makeIndex(historyArray))
     }, [])
 
     useEffect(() => {
         fetchDecks()
-        .then(decksArray => setDecks(decksArray))
+            .then(decksArray => setDecks(decksArray))
     }, [])
 
-    //Copy game history and pass it to fn
-    useEffect(() => {
-        const y = [...gameHistory]
-        makeIndex(y)
-    }, [gameHistory])
+    const findDeck = (game) => decks.find(deck => deck.id === game.card.deckId)
 
     return (
         <>
@@ -56,7 +48,12 @@ export const GameHistory = () => {
                     Recently Played
                 </p>
                 <ul className="menu-list">
-                    {gameList.map(game => <GameItem key={game.id} game={game} decks={decks} />)}
+                    {
+                        gameList.map(game => {
+                            const gameDeck = findDeck(game)
+                            return <GameItem key={game.id} game={game} gameDeck={gameDeck} />
+                        })
+                    }
                 </ul>
             </aside>
         </>
