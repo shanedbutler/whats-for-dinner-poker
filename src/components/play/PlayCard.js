1cfa5d960642ca0f153ec3e46d027ab8c5889155
Shane Butler <shanedbutler@gmail.com>
2022-08-29 14:22:45 -0400

    Match suit and display

--- src/components/play/PlayCard.js
+++ src/components/play/PlayCard.js
@@ -3,53 +3,85 @@ import "./Play.css"
 
 export const PlayCard = (props) => {
 
-    const [isHeld, setIsHeld] = useState()
+    const [isHeld, setIsHeld] = useState(false)
     const toggleHold = () => {
         setIsHeld(!isHeld)
     }
 
+    let suitIcon = ""
+    switch (props.card.suit.name) {
+        case 'Spades':
+            suitIcon = "♠️"
+            break
+        case 'Hearts':
+            suitIcon = "♥️"
+            break
+        case 'Diamonds':
+            suitIcon = "♦️"
+            break
+        case 'Clubs':
+            suitIcon = "♣"
+            break
+        default:
+            suitIcon = "X"
+    }
+
     useEffect(() => {
-
         if (isHeld) {
-
             //Copy held array state to variable
-            const heldCopy = [ ...props.held ]
+            const heldCopy = [...props.held]
 
             //Push card to array
             heldCopy.push(props.card)
             props.setHeld(heldCopy)
         }
-        else if (isHeld === false) {
-
+        else if (!isHeld) {
             //Copy held array state to variable
-            const heldCopy = [ ...props.held ]
+            const heldCopy = [...props.held]
 
             //Find held card in array and remove it
             const foundHeld = heldCopy.findIndex(heldCard => heldCard.id === props.card.id)
             heldCopy.splice(foundHeld, 1)
             props.setHeld(heldCopy)
         }
-
     }, [isHeld])
 
     return (
 
         <div className="column">
-            <div className="card meal-card">
+            <div className="card meal-card has-background-light" >
                 <div className="card-content">
                     <span className="icon">
-                        <i className="fas fa-heart"></i>
+                        {suitIcon}
                     </span>
-                    <p>
-                        {props.card.name}
-                    </p>
+                    <div className="is-flex is-justify-content-center is-align-items-center card-name">
+                        <p>
+                            {props.card.name}
+                        </p>
+                    </div>
+                    <div className="is-flex is-justify-content-right">
+                        <span className="icon">
+                            {suitIcon}
+                        </span>
+                    </div>
                 </div>
             </div>
-            <button 
-            className="button mt-1"
-            onClick={toggleHold}>
-                {!isHeld ? "Hold" : "Held"}
-            </button>
+            <section className="is-flex is-justify-content-center">
+                {
+                    isHeld ?
+                        <button
+                            className="button mt-3 is-dark"
+                            onClick={toggleHold}>
+                            Held
+                        </button>
+                        :
+                        <button
+                            className="button mt-3"
+                            onClick={toggleHold}>
+                            Hold
+                        </button>
+                }
+            </section>
         </div>
     )
 }
