ceab404ca88b7d770f73ba2808763c6cb0f72545
Shane Butler <shanedbutler@gmail.com>
2022-08-29 14:34:05 -0400

    Normalize image sizes

--- src/components/play/DeckCard.js
+++ src/components/play/DeckCard.js
@@ -5,25 +5,22 @@ import cardBackPink from "../../assets/dinner-poker-back-pink-med.png"
 export const DeckCard = (props) => {
 
     return (
-        <div className="column">
-            <div className="is-flex is-flex-direction-column">
-                <figure className="image is-237x340">
-                    {
-                        //Alternate deck image colors
-                        props.deck.id % 2 === 0 ?
-                            <img src={cardBackGreen}></img>
-                            :
-                            <img src={cardBackPink}></img>
-                    }
-                </figure>
-                <div className="is-flex is-justify-content-center">
+        <div className="column is-flex is-flex-direction-column is-align-content-center is-justify-content-center">
+
+                    <figure className="image is-237x340 card-back">
+                        {
+                            //Alternate deck image colors
+                            props.deck.id % 2 === 0 ?
+                                <img src={cardBackGreen}></img>
+                                :
+                                <img src={cardBackPink}></img>
+                        }
+                    </figure>
                     <Link to="/play/round" state={{ deckId: props.deck.id }}>
-                        <button className="button mt-1">
+                        <button className="button mt-3">
                             {props.deck.name}
                         </button>
                     </Link>
                 </div>
-            </div>
-        </div>
     )
 }
