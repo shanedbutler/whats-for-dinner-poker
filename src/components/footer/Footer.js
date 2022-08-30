58bb0e97529e61660c45f0ef0ff5246f8fbec18c
Shane Butler <shanedbutler@gmail.com>
2022-08-30 09:51:14 -0400

    Hide footer on mobile, adjust styles, change URL

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,14 +5,14 @@ export const Footer = () => {
 
     return (
         <footer className="footer">
-                <div>
+                <div className="m-2 ml-1">
                     What's for Dinner Poker
                 </div>
-                <div>
+                <div className="m-2">
                     Created by <a href="https://shanedbutler.github.io">Shane Butler</a>
                 </div>
-                <div>
-                    Source at <a href="http://opensource.org/licenses/mit-license.php">GitHub</a>
+                <div className="m-2 mr-1">
+                    Source at <a href="https://github.com/shanedbutler/whats-for-dinner-poker">GitHub</a>
                 </div>
         </footer>
     )
