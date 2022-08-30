46e3c3ea8bae5abb052f9d05ccb9d0523f519708
Shane Butler <shanedbutler@gmail.com>
2022-08-30 16:29:03 -0400

    Refactor card component to reduce use of state

--- src/components/play/PlayCard.js
+++ src/components/play/PlayCard.js
@@ -2,51 +2,24 @@ import { useEffect, useState } from "react"
 import "./Play.css"
 import { ResultCard } from "./ResultCard"
 
-export const PlayCard = (props) => {
-
-    const [isHeld, setIsHeld] = useState(false)
+export const PlayCard = ({ card, toggleHandHeld }) => {
+    const {id} = card
+    const [isHeldButton, setIsHeldButton] = useState(card.isHeld)
+    
     const toggleHold = () => {
-        setIsHeld(!isHeld)
+        setIsHeldButton(!isHeldButton)
+        toggleHandHeld(id)
     }
-
-    useEffect(() => {
-        if (isHeld) {
-            //Copy held array state to variable
-            const heldCopy = [...props.held]
-
-            //Push card to array
-            heldCopy.push(props.card)
-            props.setHeld(heldCopy)
-        }
-        else if (!isHeld) {
-            //Copy held array state to variable
-            const heldCopy = [...props.held]
-
-            //Find held card in array and remove it
-            const foundHeld = heldCopy.findIndex(heldCard => heldCard.id === props.card.id)
-            heldCopy.splice(foundHeld, 1)
-            props.setHeld(heldCopy)
-        }
-    }, [isHeld])
 
     return (
         <section className="is-flex is-flex-direction-column">
-            <ResultCard key={props.card.id} card={props.card} />
+            <ResultCard key={id} card={card} />
             <section className="is-flex is-justify-content-center">
-                {
-                    isHeld ?
-                        <button
-                            className="button mt-3 is-dark"
-                            onClick={toggleHold}>
-                            Held
-                        </button>
-                        :
-                        <button
-                            className="button mt-3"
-                            onClick={toggleHold}>
-                            Hold
-                        </button>
-                }
+                <button
+                    className={`${!isHeldButton ? "" : "is-dark"} button mt-3`}
+                    onClick={toggleHold}>
+                    {!isHeldButton ? "Hold" : "Held"}
+                </button>
             </section>
         </section>
     )
