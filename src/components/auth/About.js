f3d09be94bebb30588b66f264d543fa5bf98360c
Shane Butler <shanedbutler@gmail.com>
2022-09-01 10:36:59 -0400

    Add, edit, and style about and welcome pages

--- src/components/auth/About.js
+++ src/components/auth/About.js
@@ -7,18 +7,15 @@ export const About = () => {
         <>
             <section className="hero is-small is-primary">
                 <div className="hero-body ml-3">
-                    <p className="title">
-                        About
+                    <p className="title has-text-weight-normal">
+                        <h2>About</h2>
                     </p>
                 </div>
             </section>
-            <div>
-
-            </div>
-            <div class="columns">
+            <section class="columns">
                 <div class="column is-narrow m-5">
                 </div>
-                <div class="column content m-5">
+                <article class="column content m-5">
                     <h3>Why</h3>
                     <p>What's For Dinner Poker is a game to help plan meals for the week! Based on real life video poker, the game is played with decks of cards with each card representing a potential meal. Select a deck and play to put together a great hand and enjoy a delicious week of pre-planned eating.</p>
                     <ul>
@@ -28,16 +25,16 @@ export const About = () => {
                         <li>Share plan with others.</li>
                         <li>Have food fun!</li>
                     </ul>
-                </div>
-                <div class="column content m-5">
+                </article>
+                <article class="column content m-5">
                     <h3>How</h3>
                     <p><Link to="/register">Sign-up</Link> for a new account. Users can play the game and track their saved game results.</p>
                     <h3>Support</h3>
                     <p>What's for Dinner Poker is designed and built by <a href="https://github.com/shanedbutler">Shane Butler</a>. The web-application was initially created in the summer of 2022 as a front-end capstone project to the <a href="https://generationwv.org/programs/newforce/">NewForce</a> coding school. The app was built with React, Bulma, and SCSS. All source files are available on GitHub. To support Shane, please star the project or follow him on GitHub. Thank you and bon appetit!</p>
-                </div>
+                </article>
                 <div class="column is-narrow m-5">
                 </div>
-            </div>
+            </section>
         </>
     )
 }
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -4,11 +4,12 @@ import { GameResult } from "../history/GameResult"
 import { PlayDeck } from "../play/PlayDeck"
 import { PlayResult } from "../play/PlayResult"
 import { PlayRound } from "../play/PlayRound"
+import { Welcome } from "../welcome/Welcome"
 
 export const ApplicationViews = () => {
     return (
         <Routes>
-            <Route path="/" element={<></>} />
+            <Route path="/" element={<Welcome />} />
             <Route path="/play" element={ <PlayDeck /> } />
             <Route path="/play/round" element={ <PlayRound /> } />
             <Route path="/play/result" element={ <PlayResult /> } />
