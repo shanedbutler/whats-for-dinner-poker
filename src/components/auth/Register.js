5dda815bb1ce42ec2a664cee6bf5f40a4f2bcf7f
Shane Butler <shanedbutler@gmail.com>
2022-09-01 07:48:23 -0400

    Add page title banners

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -51,58 +51,67 @@ export const Register = () => {
     }
 
     return (
-        <main className="container is-fluid is-max-desktop columns is-centered mt-5">
-            <div className="column is-5-tablet is-5-desktop is-4-widescreen box p-5">
-                <h2 className="is-size-5">
-                    Register Player
-                </h2>
-                <form className="mt-3" onSubmit={handleRegister}>
-                    <fieldset className="field">
-                        <p className="control has-icons-left">
-                            <input className="input"
-                                type="text"
-                                placeholder="Name"
-                                onChange={evt => {
-                                    const userCopy = { ...user }
-                                    userCopy.name = evt.target.value
-                                    setUser(userCopy)
-                                }
-                                }
-                                required autoFocus />
-                            <span className="icon is-small is-left">
-                                <i className="fas fa-user"></i>
-                            </span>
-                        </p>
-                    </fieldset>
-                    <fieldset className="field">
-                        <p className="control has-icons-left">
-                            <input className="input"
-                                type="email"
-                                placeholder="Email"
-                                onChange={evt => {
-                                    const userCopy = { ...user }
-                                    userCopy.email = evt.target.value
-                                    setUser(userCopy)
-                                }
-                                }
-                                required />
-                            <span className="icon is-small is-left">
-                                <i className="fas fa-envelope"></i>
-                            </span>
-                        </p>
-                    </fieldset>
-                    <fieldset className="field is-grouped">
-                        <p className="control">
-                            <button type="submit" className="button is-success">
-                                Register
-                            </button>
-                        </p>
-                        <p className="control">
-                            <button onClick={handleCancel} className="button  is-primary">Cancel</button>
-                        </p>
-                    </fieldset>
-                </form>
-            </div>
-        </main>
-    )
+        <>
+            <section className="hero is-small is-link">
+                <div className="hero-body ml-3">
+                    <p className="title">
+                        Sign-Up
+                    </p>
+                </div>
+            </section>
+            <main className="container is-fluid is-max-desktop columns is-centered mt-5">
+                <div className="column is-5-tablet is-5-desktop is-4-widescreen box p-5">
+                    <h2 className="is-size-5">
+                        Register Player
+                    </h2>
+                    <form className="mt-3" onSubmit={handleRegister}>
+                        <fieldset className="field">
+                            <p className="control has-icons-left">
+                                <input className="input"
+                                    type="text"
+                                    placeholder="Name"
+                                    onChange={evt => {
+                                        const userCopy = { ...user }
+                                        userCopy.name = evt.target.value
+                                        setUser(userCopy)
+                                    }
+                                    }
+                                    required autoFocus />
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-user"></i>
+                                </span>
+                            </p>
+                        </fieldset>
+                        <fieldset className="field">
+                            <p className="control has-icons-left">
+                                <input className="input"
+                                    type="email"
+                                    placeholder="Email"
+                                    onChange={evt => {
+                                        const userCopy = { ...user }
+                                        userCopy.email = evt.target.value
+                                        setUser(userCopy)
+                                    }
+                                    }
+                                    required />
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-envelope"></i>
+                                </span>
+                            </p>
+                        </fieldset>
+                        <fieldset className="field is-grouped">
+                            <p className="control">
+                                <button type="submit" className="button is-success">
+                                    Register
+                                </button>
+                            </p>
+                            <p className="control">
+                                <button onClick={handleCancel} className="button  is-primary">Cancel</button>
+                            </p>
+                        </fieldset>
+                    </form>
+                </div>
+            </main>
+            </>
+            )
 }
