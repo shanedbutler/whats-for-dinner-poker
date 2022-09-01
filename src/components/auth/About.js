1db75ee9d456802bbda9ac43edc42ac9a6b3e6db
Shane Butler <shanedbutler@gmail.com>
2022-09-01 15:38:00 -0400

    Add GitHub buttons

--- src/components/auth/About.js
+++ src/components/auth/About.js
@@ -1,21 +1,22 @@
+import GitHubButton from 'react-github-btn'
+
 import { Link } from "react-router-dom"
 
 export const About = () => {
-
 
     return (
         <>
             <section className="hero is-small is-primary">
                 <div className="hero-body ml-3">
-                    <p className="title has-text-weight-normal">
-                        <h2>About</h2>
-                    </p>
+                    <h2>
+                        About
+                    </h2>
                 </div>
             </section>
-            <section class="columns">
-                <div class="column is-narrow m-5">
+            <section className="columns">
+                <div className="column is-narrow m-5">
                 </div>
-                <article class="column content m-5">
+                <article className="column content m-5">
                     <h3>Why</h3>
                     <p>What's For Dinner Poker is a game to help plan meals for the week! Based on real life video poker, the game is played with decks of cards with each card representing a potential meal. Select a deck and play to put together a great hand and enjoy a delicious week of pre-planned eating.</p>
                     <ul>
@@ -26,13 +27,21 @@ export const About = () => {
                         <li>Have food fun!</li>
                     </ul>
                 </article>
-                <article class="column content m-5">
+                <article className="column content m-5">
                     <h3>How</h3>
                     <p><Link to="/register">Sign-up</Link> for a new account. Users can play the game and track their saved game results.</p>
                     <h3>Support</h3>
                     <p>What's for Dinner Poker is designed and built by <a href="https://github.com/shanedbutler">Shane Butler</a>. The web-application was initially created in the summer of 2022 as a front-end capstone project to the <a href="https://generationwv.org/programs/newforce/">NewForce</a> coding school. The app was built with React, Bulma, and SCSS. All source files are available on GitHub. To support Shane, please star the project or follow him on GitHub. Thank you and bon appetit!</p>
+                    <p>
+                        <div className="mr-1">
+                            <GitHubButton href="https://github.com/shanedbutler/whats-for-dinner-poker" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star shanedbutler/whats-for-dinner-poker on GitHub">Star</GitHubButton>
+                        </div>
+                        <div>
+                            <GitHubButton href="https://github.com/shanedbutler" data-size="large" data-show-count="true" aria-label="Follow @shanedbutler on GitHub">Follow @shanedbutler</GitHubButton>
+                        </div>
+                    </p>
                 </article>
-                <div class="column is-narrow m-5">
+                <div className="column is-narrow m-5">
                 </div>
             </section>
         </>
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,3 +1,5 @@
+import GitHubButton from 'react-github-btn'
+
 export const Welcome = () => {
 
     return (
@@ -12,18 +14,26 @@ export const Welcome = () => {
                     </p>
                 </div>
             </section>
-            <section class="columns">
-                <div class="column is-narrow m-5 is-hidden-mobile">
+            <section className="columns">
+                <div className="column is-narrow m-5 is-hidden-mobile">
                 </div>
-                <article class="column content m-5">
+                <article className="column content m-5">
                     <h3>How</h3>
-                    <p>What's for Dinner Poker is easy! Navigate to play, choose a deck, and draw. You will be dealt five dinner cards. Use the hold buttons to keep a card in your hand through the next draw. See something you'd like to eat? Hold it!<br/> You will have three rounds to make your final hand. The final five cards at the end of the game make your meal plan! 🎊 <br/>Copy the results to your clipboard, save them to your game history list, or just keep playing!</p>
+                    <p>What's for Dinner Poker is easy! Navigate to play, choose a deck, and draw. You will be dealt five dinner cards. Use the hold buttons to keep a card in your hand through the next draw. See something you'd like to eat this week? Hold it!<br /> You will have three rounds to make your final hand. The final five cards at the end of the game make up your meal plan! 🎊 <br />Copy the results to your clipboard, save them to your game history list, or just keep playing!</p>
                     <h3>About</h3>
                     <p>What's for Dinner Poker is designed and built by <a href="https://github.com/shanedbutler">Shane Butler</a>. The web-application was initially created in the summer of 2022 as a front-end capstone project to the <a href="https://generationwv.org/programs/newforce/">NewForce</a> coding school. The app was built with React, Bulma, and SCSS. All source files are available on GitHub. To support Shane, please star the project or follow him on GitHub.
                     </p>
                     <p>Thank you and bon appétit!</p>
+                    <p>
+                        <div className="mr-1">
+                            <GitHubButton href="https://github.com/shanedbutler/whats-for-dinner-poker" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star shanedbutler/whats-for-dinner-poker on GitHub">Star</GitHubButton>
+                        </div>
+                        <div>
+                            <GitHubButton href="https://github.com/shanedbutler" data-size="large" data-show-count="true" aria-label="Follow @shanedbutler on GitHub">Follow @shanedbutler</GitHubButton>
+                        </div>
+                    </p>
                 </article>
-                <article class="column content m-5">
+                <article className="column content m-5">
                     <h3>Features</h3>
                     <p>There are two decks based on seasonal eating. Each deck holds 52 dinner cards.</p>
                     <ul>
@@ -40,7 +50,7 @@ export const Welcome = () => {
                     <p><strong>Vegetarian?</strong> Enable vegetarian mode to play only with cards that can be prepared without meat!</p>
                     <p>More decks, cards, and features are planned!</p>
                 </article>
-                <div class="column is-narrow m-5 is-hidden-mobile">
+                <div className="column is-narrow m-5 is-hidden-mobile">
                 </div>
             </section>
         </>
