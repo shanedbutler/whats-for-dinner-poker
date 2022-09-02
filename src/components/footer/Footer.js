684766f10a48d2bd98670276390127bd74848b36
Shane Butler <shanedbutler@gmail.com>
2022-09-02 07:38:57 -0400

    Footer style adjustments

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,16 +4,22 @@ export const Footer = () => {
 
 
     return (
-        <footer className="footer">
-                <div className="m-2 ml-1">
+        <footer className="site-footer">
+            <div className="footer-flex">
+                <div>
+                </div>
+                <div className="m-2 ml-3">
                     What's for Dinner Poker
                 </div>
                 <div className="m-2">
                     Created by <a href="https://shanedbutler.github.io">Shane Butler</a>
                 </div>
-                <div className="m-2 mr-1">
+                <div className="m-2 mr-5">
                     Source at <a href="https://github.com/shanedbutler/whats-for-dinner-poker">GitHub</a>
                 </div>
+                <div>
+                </div>
+            </div>
         </footer>
     )
 }
