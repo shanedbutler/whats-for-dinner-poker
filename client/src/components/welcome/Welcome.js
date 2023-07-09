22d613f233bf4bdc9340b1bd15d84f9f5c13cf34
Shane Butler <shanedbutler@gmail.com>
2023-07-09 17:31:23 -0400

    Update welcome component to be more informational

--- client/src/components/welcome/Welcome.js
+++ client/src/components/welcome/Welcome.js
@@ -18,24 +18,27 @@ export const Welcome = () => {
                 <div className="column is-narrow m-5 is-hidden-mobile">
                 </div>
                 <article className="column content m-5">
-                    <h3>How</h3>
+                <h3>About</h3>
                     <p>
-                        What's for Dinner Poker is easy to learn! Navigate to play, choose a deck, and draw.
-                        The deck is shuffled and you are dealt five dinner cards.
-                        Use the hold buttons to keep a card in your hand through the next draw.
-                        See something you'd like to eat this week? Hold it!<br />
-                        You will have three rounds to make your final hand. The final five cards at the end
-                        of the game make up your meal plan! 🎊 <br />Copy the results to your clipboard,
-                        save them to your game history list, or just keep playing!</p>
-                    <h3>About</h3>
-                    <p>
-                        What's for Dinner Poker is designed and built by <a href="https://github.com/shanedbutler">Shane Butler</a>.
-                        The web-application was initially created in the summer of 2022 as a front-end capstone project
-                        to the <a href="https://generationwv.org/programs/newforce/">NewForce</a> coding school. The
-                        app was built with React, Bulma, and SCSS. All source files are available on GitHub. To support
-                        Shane, please star the project or follow him on GitHub.
+                        What's For Dinner Poker is a game to help plan meals for the week! Based on real life video poker,
+                        the game is played with decks of cards with each card representing a potential meal.
+                        Select a deck and play to put together a great hand and enjoy a delicious week of pre-planned eating.
                     </p>
-                    <p>Thank you and bon appétit!</p>
+                    <ul>
+                        <li>Reduce decision fatigue</li>
+                        <li>Try something new</li>
+                        <li>Eat seasonally</li>
+                        <li>Share plan with others</li>
+                        <li>Have food fun!</li>
+                    </ul>
+                    <p>
+                        This web application is designed and built by <a href="https://github.com/shanedbutler">Shane Butler</a>.
+                        It was initially created in the summer of 2022 as a front-end capstone project
+                        to the <a href="https://generationwv.org/programs/newforce/">NewForce</a> coding school. All source files are available on GitHub.
+                    </p>
+                    <p>
+                        Thank you and bon appétit!
+                    </p>
                     <div>
                         <div className="mr-1">
                             <GitHubButton
@@ -61,16 +64,26 @@ export const Welcome = () => {
                     </div>
                 </article>
                 <article className="column content m-5">
+                <h3>How</h3>
+                    <p>
+                        To being, navigate to play, choose a deck, and draw.
+                        The deck is shuffled and you are dealt five dinner cards.
+                        See something you'd like to eat this week?
+                    </p>
+                    <p>
+                        <strong>Hold it!</strong> It will stay in your hand through the next draw. 💎🤲
+                    </p>
+                    <p>
+                        You will have three rounds to make your final hand. The final five cards at the end
+                        of the game make up your meal plan! 🎊 
+                    </p>
+                    <p>
+                        Copy the results to your clipboard,
+                        save them to your game history list, or just keep playing!
+                    </p>
                     <h3>
                         Features
                     </h3>
-                    <p>
-                        There are two decks based on seasonal eating. Each deck holds 52 dinner cards.
-                    </p>
-                    <ul>
-                        <li>Summer</li>
-                        <li>Winter</li>
-                    </ul>
                     <p>
                         Each dinner card belongs to one of four suits, hover over them during the game to see their description.
                     </p>
@@ -82,9 +95,6 @@ export const Welcome = () => {
                     </ul>
                     <p>
                         <strong>Vegetarian?</strong> Enable vegetarian mode to play only with cards that can be prepared without meat!
-                    </p>
-                    <p>
-                        More decks, cards, and features are planned!
                     </p>
                 </article>
                 <div className="column is-narrow m-5 is-hidden-mobile">
