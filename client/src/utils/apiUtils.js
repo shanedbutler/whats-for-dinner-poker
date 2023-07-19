a6b6ae5ffddbcc76ef24ce94234d06d365013946
Shane Butler <shanedbutler@gmail.com>
2023-07-19 15:56:40 -0400

    Remove unused fns and refactor existing to async

--- client/src/utils/apiUtils.js
+++ client/src/utils/apiUtils.js
@@ -2,61 +2,20 @@
 
 const API = "http://localhost:8088"
 
-export const fetchUsers = (resource = "", options = {}) => {
-    const endPoint = "/users"
-    return fetch(`${API}${endPoint}${resource}`, options)
-    .then(response => response.json())
-}
-
-export const fetchDecks = (resource = "", options = {}) => {
+export const fetchDecks = async (resource = "", options = {}) => {
   const endPoint = "/decks"
-    return fetch(`${API}${endPoint}${resource}`, options)
-    .then(response => response.json())
+    const response = await fetch(`${API}${endPoint}${resource}`, options)
+  return await response.json()
 }
 
-export const fetchSuits = (resource = "", options = {}) => {
+export const fetchSuits = async (resource = "", options = {}) => {
   const endPoint = "/suits"
-    return fetch(`${API}${endPoint}${resource}`, options)
-    .then(response => response.json())
+    const response = await fetch(`${API}${endPoint}${resource}`, options)
+  return await response.json()
 }
 
-export const fetchCards = (resource = "", options = {}) => {
+export const fetchCards = async (resource = "", options = {}) => {
   const endPoint = "/cards"
-    return fetch(`${API}${endPoint}${resource}`, options)
-    .then(response => response.json())
+    const response = await fetch(`${API}${endPoint}${resource}`, options)
+  return await response.json()
 }
-
-export const fetchHistory = (resource = "", options = {}) => {
-  const endPoint = "/gameHistory"
-    return fetch(`${API}${endPoint}${resource}`, options)
-    .then(response => response.json())
-}
-
-export const postOption = (bodyContent) => {
-    const post = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(bodyContent),
-      }
-  return post
-}
-
-export const putOption = (bodyContent) => {
-    const put = {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(bodyContent),
-      }
-    return put
-  }
-
-  export const deleteOption = () => {
-    const deleteOpt = {
-        method: "DELETE",
-    }
-    return deleteOpt
-  }
