78a4dc37fb0cde06650ab98c3bf8bac06fc93239
Shane Butler <shanedbutler@gmail.com>
2023-08-17 15:24:28 -0400

    Remove game history routes

--- client/src/components/views/ApplicationViews.js
+++ client/src/components/views/ApplicationViews.js
@@ -1,6 +1,4 @@
 import { Route, Routes } from "react-router-dom"
-import { GameHistory } from "../history/GameHistory"
-import { GameResult } from "../history/GameResult"
 import { PlayDeck } from "../play/PlayDeck"
 import { PlayResult } from "../play/PlayResult"
 import { PlayRound } from "../play/PlayRound"
@@ -13,8 +11,6 @@ export const ApplicationViews = () => {
             <Route path="/play" element={ <PlayDeck /> } />
             <Route path="/play/round" element={ <PlayRound /> } />
             <Route path="/play/result/:veg" element={ <PlayResult /> } />
-            <Route path="/history" element={ <GameHistory /> } />
-            <Route path="/history/:id" element={ <GameResult /> } />
         </Routes>
     )
 }
